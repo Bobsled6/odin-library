18e89906ad403c60ddfa49c2f664e3260fe41e16
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-02-13 14:46:30 -0500

    added event listener to submit button to add the book to library, changed Book constructor and addBookToLibrary function to use the data from the form to create the book object and push it into the myLibrary array

--- library.js
+++ library.js
@@ -1,35 +1,53 @@
 const button = document.getElementById(newBookButton);
 const dialog = document.querySelector("dialog");
-
+const bookForm = document.getElementById("form");
 const myLibrary = [];
 
-function Book(author, title, pages, read) {
+
+
+function Book() {
     if (!new.target) {
         throw error("You must use the new operator to call the constructor")
     }
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return this.author + "'s " + this.title + ", " + this.pages + " pages, " + this.read
-    }
+    let bookData = new FormData(bookForm);
+    bookAuthor = bookData.get("author");
+    bookTitle = bookData.get("title");
+    bookPages = bookData.get("pages");
+    bookRead = bookData.get("read");
+    this.author = bookAuthor;
+    this.title = bookTitle;
+    this.pages = bookPages;
+    this.read = bookRead;
+    this.id = crypto.randomUUID();
+    
+    // this.info = function() {
+    //    return this.author + "'s " + this.title + ", " + this.pages + " pages, " + this.read
+    // }
 
 }
 
-function addBookToLibrary(author, title, pages, read) { 
-    let libraryBook = new Book(author, title, pages, read);
-    myLibrary.push(libraryBook) ;
-
-}
-
-addBookToLibrary('J.R.R. Tolkien', 'The Hobbit', '245', 'not read');
-addBookToLibrary('J.K. Rowling', 'Harry Potter', '256', 'read');
-
-console.log(myLibrary);
+function addBookToLibrary(){
+    let libraryBook = new Book();
+    myLibrary.push(libraryBook);
+    console.log(myLibrary);
+};
 
 
 newBookButton.addEventListener("click", () => {
     dialog.showModal();
 });
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+    dialog.close();
+    bookForm.reset();
+});
+    
+
+
+
+
+
+
 
