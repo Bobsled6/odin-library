045593039cfdd9f1d9be490c1d5b99f1b2a2d7be
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-07-02 11:24:00 -0400

    added Table class, added genTable() as class method for table, fixed sortArrayPages() to compare number values

--- library.js
+++ library.js
@@ -16,7 +16,6 @@ const bookTable = document.getElementById("bookTable");
 class Book {
     
     constructor(bookData) {
-        this.bookData = new FormData(bookForm);
         this.author = bookData.get("author");
         this.title = bookData.get("title");
         this.pages = bookData.get("pages");
@@ -25,19 +24,26 @@ class Book {
     }
 
     addBookToLibrary(){
-        let libraryBook = this;
-        myLibrary.push(libraryBook);
-        console.log(myLibrary);
+        myLibrary.push(this);
         for (let i = 0; i < myLibrary.length; i++){   
             document
             .getElementById("tableDiv")
-            .innerHTML = genTable(myLibrary);
+            .innerHTML = new Table(myLibrary).genTable();
             clickEvents();
-        }};
+        } console.log(myLibrary)};
 }
 
-function genTable(myLibrary) {
-    let bookTable = `
+
+
+class Table {
+    
+    constructor(myLibrary) {
+        this.myLibrary = myLibrary;
+    }
+
+    genTable() {
+        let currentLibrary = this.myLibrary;
+        let bookTable = `
         <table id = "bookTable">
             <tr>
                 <th id = "titleHeader"> Title </th>
@@ -46,21 +52,22 @@ function genTable(myLibrary) {
                 <th id = "readHeader"> Read? </th>
                 <th id = "removeHeader">Remove </th>
             </tr>`;
-    for (const book of myLibrary) {
-        bookTable += `
-        <div class = "tableRowDiv">    
-        <tr>
-                <td> ${book.title} </td>
-                <td> ${book.author} </td>
-                <td> ${book.pages} </td>
-                <td> <input  class = "readCheckbox" id = "${book["data-id"] + 1}" type = "checkbox"> </td>
-                <td> <button class = "removeButton" id = "${book["data-id"]}"> X </button> </td>
-            </tr> 
-        </div>`;
+        for (const book of currentLibrary) {
+            bookTable += `
+            <div class = "tableRowDiv">    
+                <tr>
+                    <td> ${book.title} </td>
+                    <td> ${book.author} </td>
+                    <td> ${book.pages} </td>
+                    <td> <input  class = "readCheckbox" id = "${book["data-id"] + 1}" type = "checkbox"> </td>
+                    <td> <button class = "removeButton" id = "${book["data-id"]}"> X </button> </td>
+                </tr> 
+            </div>`;
+        }
+        bookTable += "</table>";
+        return bookTable;
     }
-    bookTable += "</table>";
-    return bookTable;
-};
+}
 
 
 newBookButton.addEventListener("click", () => {
@@ -73,6 +80,7 @@ form.addEventListener("submit", (event) => {
     dialog.close();
     bookForm.reset();
 });
+
 
 
 sortArrayTitle = function () {
@@ -103,16 +111,18 @@ sortArrayAuthor = function () {
 
 sortArrayPages = function () {
      myLibrary.sort((a, b) => {
-        const pagesA = a.pages;
-        const pagesB = b.pages;
+        const pagesA = Number(a.pages);
+        const pagesB = Number(b.pages);
         if (pagesA < pagesB) {
-        return -1;
-    }
+            return -1;
+        }
         if (pagesA > pagesB) {
-        return 1;
-  }
+            return 1;
+        }
         return 0;
-})}
+    })
+    console.log(myLibrary)
+}
 
 clickEvents = function(){
     const authorHeader = document.getElementById("authorHeader");
@@ -122,59 +132,57 @@ clickEvents = function(){
     const readCheckbox = document.querySelectorAll('input.readCheckbox');
 
      titleHeader.addEventListener("click", () => {
-
         sortArrayTitle();
         document
         .getElementById("tableDiv")
-        .innerHTML = genTable(myLibrary);
-         clickEvents();
+        .innerHTML = new Table(myLibrary).genTable();
+        clickEvents();
 })
      authorHeader.addEventListener("click", () => {
         sortArrayAuthor();
         document
         .getElementById("tableDiv")
-        .innerHTML = genTable(myLibrary);
+        .innerHTML = new Table(myLibrary).genTable();
         clickEvents();
 })
 
       pagesHeader.addEventListener("click", () => {
         sortArrayPages();
+        console.log(myLibrary)
         document
         .getElementById("tableDiv")
-        .innerHTML = genTable(myLibrary);
+        .innerHTML = new Table(myLibrary).genTable();
         clickEvents();
 })
      removeButton.forEach((btn) => {
         
         btn.addEventListener("click", () => {
-             let remove = myLibrary.findIndex(myLibrary => myLibrary["data-id"] === btn.id); 
-        if (remove !== -1) {
-            myLibrary.splice(remove, 1);
-        }
-        document
-        .getElementById("tableDiv")
-        .innerHTML = genTable(myLibrary);
-        clickEvents();
-})
-})
+            let remove = myLibrary.findIndex(myLibrary => myLibrary["data-id"] === btn.id); 
+            if (remove !== -1) {
+                myLibrary.splice(remove, 1);
+            }
+            document
+            .getElementById("tableDiv")
+            .innerHTML = new Table(myLibrary).genTable();
+            clickEvents();
+        })
+    })
 
     readCheckbox.forEach ((cbx) => {
         let checkBoxTest = myLibrary.findIndex(myLibrary => myLibrary["data-id"] + 1 === cbx.id);
         if (myLibrary[checkBoxTest].read === "on") {
-        cbx.checked = true;
-       }
-       else {
-        cbx.checked = false;
-       }
-
-       cbx.addEventListener ("change", () => {
-        if(myLibrary[checkBoxTest].read === "on") {
-            myLibrary[checkBoxTest].read = "null"
+            cbx.checked = true;
         }
-        else {myLibrary[checkBoxTest].read = "on"}
-       })
-       
- })
+        else {
+            cbx.checked = false;
+        }
+        cbx.addEventListener ("change", () => {
+            if(myLibrary[checkBoxTest].read === "on") {
+                myLibrary[checkBoxTest].read = "null"
+            }
+            else {myLibrary[checkBoxTest].read = "on"}
+        })
+    })
       
 };
 
